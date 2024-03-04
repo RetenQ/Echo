d838835e477264734b3d8e4662b58d709903c033
Hongyi.Lan <18965093000@163.com>
2024-03-04 15:37:59 +0800

    暂存

--- Assets/Scripts/Managers/RhythmMgr.cs
+++ Assets/Scripts/Managers/RhythmMgr.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RhythmMgr : MonoBehaviour
+// 管理BGM的东西
+public class RhythmMgr : SingletonMono<RhythmMgr>
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,28 @@ public class RhythmMgr : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    public void PlayerRhyOn()
+    {
+
+    }
+
+    public void PlayerRhyOff()
+    {
+
+    }
+
+    /// <summary>
+    /// 通知所有的，由AI控制的，需要在节奏点上进行操作的Object进行响应
+    /// </summary>
+    public void SystemRhy()
+    {
+
     }
 }
--- Assets/Scripts/UsingObj/BaseObj.cs
+++ Assets/Scripts/UsingObj/BaseObj.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class BaseObj : MonoBehaviour
 {
+    [Header("基础数值")]
+    public float maxHp;
+    public float nowHp;
+
+    public float attack; // 攻击力，这里就是直接表示成功命中会有多少伤害 
+    public float speed;
+
+    private void Awake()
+    {
+        nowHp = maxHp;  // 初始生命即为最大值
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,28 @@ public class BaseObj : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(nowHp <= 0)
+        {
+            Death();
+        }
+
+        ObjUpdate();
     }
+
+    public virtual void ObjUpdate()
+    {
+        // 每一个Obj自己的Update
+
+    }
+
+    public virtual void Death()
+    {
+
+    }
+
+    public virtual void Hurt(float _damage)
+    {
+        nowHp = _damage;
+    }
+    
 }
